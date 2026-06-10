0bf21b2918655d1006410059acdaed2e558e8c3e
Dmytro Fomin <fodi85999@gmail.com>
2026-06-10 11:14:10 +0200

    Redirect old avocado slug

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   async redirects() {
     return [
+      {
+        source: '/:locale/skladniki/-avocado-k-c-b6-avocado-is-a-creamy-fruit-native-to-central-america-prized-for-its-healthy-monounsaturated-fats-rich-in-potassium-more-than-bananas-folate-and-vitamins-k-c-and-b6',
+        destination: '/:locale/skladniki/avocado',
+        permanent: true
+      },
       { source: '/blog/:path*', destination: '/pl/blog/:path*', permanent: true },
       { source: '/sklep/:path*', destination: '/pl/sklep/:path*', permanent: true },
       { source: '/skladniki/:path*', destination: '/pl/skladniki/:path*', permanent: true },
